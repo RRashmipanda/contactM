d0c01d1c288313aa8b50e3e4535cd4e814b650eb
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-08-11 21:21:21 +0530

    login form updated

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,15 @@
-import styles from "./login.module.css"
+import LoginForm from "@/app/_components/LoginForm";
+import React from "react";
 
 const login = () => {
  
   return (
-    <div className={styles.login}>Login Page</div>
+    <div className = "max-w-md mx-auto bg-amber-200 p-8 rounded-lg shadow-md">
+       <h1 className="text-2xl font-bold mb-6">Login</h1>
+
+   <LoginForm/>
+
+   </div>
   )
 }
 
