a49e0eac4ea82865595779d92ec32ecccb7baeab
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-08-12 16:06:35 +0530

    auth server action updated

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,20 @@
 import LoginForm from "@/app/_components/LoginForm";
+import Link from "next/link";
 import React from "react";
 
 const login = () => {
  
   return (
-    <div className = "max-w-md mx-auto bg-amber-200 p-8 rounded-lg shadow-md">
-       <h1 className="text-2xl font-bold mb-6">Login</h1>
-
+    <div className = "max-w-md mt-5 mx-auto bg-amber-100 p-8 rounded-lg shadow-md">
+      
+       
    <LoginForm/>
-
+ <p className="mt-4 text-center">
+  Don't have an account ? {""}
+  <Link href="/register" className="text-blue-600 hover:underline">
+  Register
+  </Link>
+ </p>
    </div>
   )
 }
--- src/app/_components/LoginForm.tsx
+++ src/app/_components/LoginForm.tsx
@@ -1,9 +1,51 @@
-"use client"
+"use client";
+
+import { loginAction } from "@/actions/auth";
+
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
-}
 
-export default LoginForm
+  return (
+   
+      <form action={loginAction}
+        className="w-full max-w-sm rounded-2xl bg-white p-6 shadow-md"
+      >
+        <h2 className="mb-4 text-center text-2xl font-bold">Login</h2>
+
+        
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            className="mt-1 w-full rounded-lg border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"
+          />
+          
+        </div>
+
+        
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            className="mt-1 w-full rounded-lg border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"
+          />
+          
+        </div>
+
+      
+        <button
+          type="submit"          
+          className="w-full rounded-lg bg-blue-500 py-2 text-white transition hover:bg-blue-700 disabled:opacity-50"
+        >
+          Login
+        </button>
+      </form>
+    
+  );
+};
+
+export default LoginForm;
